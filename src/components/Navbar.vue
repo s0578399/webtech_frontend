5edd33cfb5f504ac5eb5939e61b44f1c2e45a249
s0578399 <77054860+s0578399@users.noreply.github.com>
2021-12-05 14:35:10 +0100

    Add Navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Shop</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <router-link class="nav-link" to="/">Home</router-link>
+          <router-link class="nav-link" to="/about">Über uns</router-link>
+          <router-link class="nav-link" to="/products">Produkte</router-link>
+          <router-link class="nav-link" to="/categories">Kategorien</router-link>
+<!--          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link" href="#">About</a>
+          <a class="nav-link" href="#">Produkte</a>
+          <a class="nav-link disabled">Kategorien</a>-->
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Products.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="products">
+    <h1>Willkommen zu den Produkten</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Products'
+}
+</script>
+
+<style scoped>
+
+</style>
