587b5232415892bc0e51136bd3ca8861f5d185af
s0578399 <77054860+s0578399@users.noreply.github.com>
2021-12-05 14:34:38 +0100

    Add Hello World

---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import About from '../views/About'
+import Products from '../views/Products'
+import Categories from '../views/Categories'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  },
+  {
+    path: '/products',
+    name: 'Products',
+    component: Products
+  },
+  {
+    path: '/categories',
+    name: 'Categories',
+    component: Categories
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  linkActiveClass: 'active',
+  routes
+})
+
+export default router
