041ea9b7263c76e2b469a2321d3c07db8d58bd7e
s0578399 <77054860+s0578399@users.noreply.github.com>
2022-01-09 22:42:18 +0100

    add order single view

---
+++ src/views/orderSingleView.vue
@@ -0,0 +1,21 @@
+<template>
+  <tr v-for="order in orders" :key="order.id">
+    <router-link to="/home">
+      <td>{{ order.id }}</td>
+    </router-link>
+    <td width="100%">{{ order.surname }}</td>
+    <td width="100%">{{ order.lastname }}</td>
+    <td width="250%">{{ order.city }}</td>
+    <td width="100%">{{ calculateOrderValue(order) }}€</td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: 'orderSingleView'
+}
+</script>
+
+<style scoped>
+
+</style>
