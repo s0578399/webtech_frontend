8021d551a6cd33c39658c25b8da56196d148218f
s0578399 <77054860+s0578399@users.noreply.github.com>
2022-01-09 10:48:53 +0100

    add deleteProduct mutation

--- src/store/index.js
+++ src/store/index.js
@@ -30,6 +30,9 @@ export default createStore({
   mutations: {
     addProduct (state, product) {
       state.productsInShoppingCart.push(product)
+    },
+    deleteProduct (state, product, i) {
+      state.productsInShoppingCart.splice(i, 1)
     }
   },
   actions: {},
