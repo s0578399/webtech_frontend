46ff1f26c0aedfc50eacfd9aba9996bdc081c1db
s0578399 <77054860+s0578399@users.noreply.github.com>
2022-01-09 16:02:22 +0100

    add create product page

---
+++ src/views/CreateProduct.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="container pt-5 pb-5">
+    <h3>Produkt erstellen</h3>
+    <hr class="hr mx-auto">
+    <p>Hier kann ein neues Produkt erstellt werden</p>
+    <div class="container">
+      <ProductForm :create-or-update="create"></ProductForm>
+    </div>
+  </section>
+</template>
+
+<script>
+import ProductForm from '@/components/ProductForm'
+export default {
+  name: 'CreateProduct',
+  components: { ProductForm },
+  data () {
+    return {
+      create: 'create'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.hr {
+  width: 300px;
+  height: 3px;
+  background-color: darkgreen;
+  position: center;
+}
+.container{
+  padding-bottom: 100px;
+}
+</style>
